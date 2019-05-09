8dddb8fffbfeecb6d1012044eceb8f5f911aebe4
Jiehan Wang <jiehanwang@berkeley.edu>
2019-05-09 15:31:17 -0700

    local change

--- volumetric/src/pathtracer.cpp
+++ volumetric/src/pathtracer.cpp
@@ -24,11 +24,11 @@ using std::min;
 using std::max;
 
 //Coefficients used for volumetric rendering
-double EXTINCTION = 0.1;
+double EXTINCTION = 0.4;
 double SCATTERING = 0.5;
 //Instantiate a Henyey-Greenstein phase function coefficient
 // -1 <= G <= 1
-double G = 0.5;
+double G = 0.3;
 
 namespace CGL {
 
